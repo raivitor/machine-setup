ba81b222ef16ec0392760776fdf5c810ce815b37
Raí Vitor <raivitor@gmail.com>
2022-12-17 19:28:43 -0300

    removed docker-compose

--- setup.sh
+++ setup.sh
@@ -81,11 +81,5 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
-sudo groupadd docker
-sudo usermod -aG docker $USER
-
-echo 'installing docker-compose' 
-sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
 docker --version
-docker-compose --version
+docker compose version
