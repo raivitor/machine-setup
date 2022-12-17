16dfb51a082701e8a8d4beace45b5b3c5139e52e
Raí Vitor <raivitor@gmail.com>
2022-12-17 18:59:40 -0300

    updated version nvm and node

--- setup.sh
+++ setup.sh
@@ -59,14 +59,14 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 echo 'installing nvm' 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 nvm --version
-nvm install 16
-nvm alias default 16
+nvm install 18
+nvm alias default 18
 node --version
 npm --version
 
