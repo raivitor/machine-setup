7706c3cff0964feb44da5e106ab8065543e1dd89
Raí Vitor <raivitor@gmail.com>
2022-12-17 18:32:20 -0300

    updated spotify

--- setup.sh
+++ setup.sh
@@ -27,7 +27,9 @@ sudo rm /etc/apt/preferences.d/nosnap.pref
 sudo apt install snapd
 
 echo 'installing spotify' 
-sudo snap install spotify
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client -y
 
 echo 'installing discord' 
 sudo snap install discord
